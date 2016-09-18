994ff1f18ea3428f024f7cd61f63cc39a9a1138d
David M <davidbepic@gmail.com>
2016-09-17 22:28:13 -0400

    Files added for site

---
+++ 1.js
@@ -0,0 +1,8 @@
+/*$(window).scroll(function() {
+    $("#p1").css({
+        'transform': 'translate(0px, ' + $(window).scrollTop() + '%)'
+    });
+    $("#bttn1").css({
+        'transform': 'translate(0px, ' + $(window).scrollTop() + '%)'
+    });
+});*/
