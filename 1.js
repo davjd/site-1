64a1cbdfde540768aa708d3086a712a32b253a9d
David M <davidbepic@gmail.com>
2017-01-24 02:14:09 -0500

    Delete 1.js

--- 1.js
+++
@@ -1,8 +0,0 @@
-/*$(window).scroll(function() {
-    $("#p1").css({
-        'transform': 'translate(0px, ' + $(window).scrollTop() + '%)'
-    });
-    $("#bttn1").css({
-        'transform': 'translate(0px, ' + $(window).scrollTop() + '%)'
-    });
-});*/
